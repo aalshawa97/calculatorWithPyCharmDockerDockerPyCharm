5483ec9cb8ca69545bfe759b5a181f0ddd96c26f
aalshawa97 <abdullahalshawa@gmail.com>
2023-05-22 12:03:02 -0700

    Simple GET request from the AWS homepage

--- aws.py
+++ aws.py
@@ -1,34 +1,8 @@
-import boto3
+import urllib.request
 
-def create_s3_bucket(bucket_name):
-    """
-    Create an S3 bucket with the specified name.
-    """
-    s3 = boto3.client('s3')
-    s3.create_bucket(Bucket=bucket_name)
-    print(f"Creating bucket '{bucket_name}'.")
+# Perform a simple GET request to the AWS homepage
+response = urllib.request.urlopen('https://aws.amazon.com')
 
-def upload_file_to_s3(file_path, bucket_name, object_key):
-    """
-    Upload a file to the specified S3 bucket.
-    """
-    s3 = boto3.client('s3')
-    s3.upload_file(file_path, bucket_name, object_key)
-    print(f"Uploaded file '{file_path}' to bucket '{bucket_name}' with object key '{object_key}'.")
-
-# Specify the bucket name
-bucket_name = 'exam-prep-bucket-2023'
-
-# Create the S3 bucket
-#create_s3_bucket(bucket_name)
-
-# Upload a file to the S3 bucket
-file_path = r"C:\Users\15039\Desktop\Images\python.jpg"
-object_key = 'python.jpg'
-
-# Upload the file
-#upload_file_to_s3(file_path, bucket_name, object_key)
-
-print(f"Uploading file '{file_path}' to bucket '{bucket_name}' with object key '{object_key}'.")
-# Print a success message
-#print(f" Creating bucket '{bucket_name}'.")
+# Read and print the response
+data = response.read()
+print(data)
