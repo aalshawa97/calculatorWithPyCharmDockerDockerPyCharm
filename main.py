810f8ee6c55bd65f0d500940f5c7bf2ede516bff
abdullah97 <abdullahalshawa@gmail.com>
2023-06-29 10:01:32 -0700

    Instructions have been added for the * and ** operators.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#Abdullah Mutaz Alshawa
+# Abdullah Mutaz Alshawa
 import argparse
 import numpy as np
-
+print("Please note that to use * and ** single quotes must surround the operator.")
+print("For example, python main.py '**' 2.0 2.0 will return 4.0 which is 2^2.")
 parser = argparse.ArgumentParser()
-parser.add_argument("operation", help="Operation to perform (+,-,*,/, %)")
+parser.add_argument("operation", help="Operation to perform (+, -, *, /, %, **)")
 parser.add_argument("num1", type=float, help="First number")
 parser.add_argument("num2", type=float, help="Second number")
 args = parser.parse_args()
@@ -18,6 +19,10 @@ elif args.operation == '/':
     result = np.divide(args.num1, args.num2)
 elif args.operation == '%':
     result = args.num1 % args.num2
+elif args.operation == '**':
+    result = np.power(args.num1, args.num2)
+elif args.operation == '//':
+    result = args.num1 // args.num2
 else:
     raise ValueError("Invalid operation")
 
